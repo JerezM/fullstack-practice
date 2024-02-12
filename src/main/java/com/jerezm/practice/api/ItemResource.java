3a9ba2e3ccf296de7e8c4a6f5436f6c28de30faf
Martin Jerez <jerez149@gmail.com>
2024-02-11 23:37:10 -0300

    get all items and create item method added

--- src/main/java/com/jerezm/practice/api/ItemResource.java
+++ src/main/java/com/jerezm/practice/api/ItemResource.java
@@ -1,26 +1,36 @@
 package com.jerezm.practice.api;
 
+import com.jerezm.practice.dto.ItemDTO;
+import com.jerezm.practice.model.Item;
 import com.jerezm.practice.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("items")
 public class ItemResource {
-    private ItemService itemService;
+    private final ItemService itemService;
 
     @Autowired
     public ItemResource(ItemService itemService) {
         this.itemService = itemService;
     }
 
-    @GetMapping("")
-    public ResponseEntity<String> getItems() {
-        String items = itemService.getItems();
-        return new ResponseEntity<>(items, HttpStatusCode.valueOf(200));
+    @GetMapping
+    public ResponseEntity<List<ItemDTO>> getItems() {
+        List<ItemDTO> itemsDTO = itemService.getItems();
+
+        return new ResponseEntity<>(itemsDTO, HttpStatusCode.valueOf(200));
+    }
+
+    @PostMapping
+    public ResponseEntity<ItemDTO> createItem(@RequestBody ItemDTO itemDTO) {
+        ItemDTO itemDTOCreated = itemService.createItem(itemDTO);
+        return new ResponseEntity<>(itemDTOCreated, HttpStatus.CREATED);
     }
 }
--- src/main/java/com/jerezm/practice/service/ItemService.java
+++ src/main/java/com/jerezm/practice/service/ItemService.java
@@ -1,21 +1,41 @@
 package com.jerezm.practice.service;
 
+import com.jerezm.practice.dto.ItemDTO;
+import com.jerezm.practice.model.Item;
+import com.jerezm.practice.utils.DTOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.jerezm.practice.repository.ItemRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ItemService {
 
-    private ItemRepository itemRepository;
+    private final ItemRepository itemRepository;
 
     @Autowired
     public ItemService(ItemRepository itemRepository){
         this.itemRepository = itemRepository;
     }
 
+    @Transactional(readOnly = true)
+    public List<ItemDTO> getItems(){
+        List<Item> items = itemRepository.findAll();
+        List<ItemDTO> itemsDTO = new ArrayList<>();
 
-    public String getItems(){
-        return "Hello World!";
+        items.forEach(item -> itemsDTO.add(DTOUtils.toDTO(item)));
+
+        return itemsDTO;
+    }
+
+    @Transactional
+    public ItemDTO createItem(ItemDTO itemDTO) {
+        Item itemCreated = DTOUtils.fromDTO(itemDTO);
+        itemCreated = itemRepository.save(itemCreated);
+
+        return DTOUtils.toDTO(itemCreated);
     }
 }
